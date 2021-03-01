7e520496d16949c8c7fe9c5e03fc806e82ea6454
AWayzy <77026218+AWayzy@users.noreply.github.com>
2021-03-01 11:49:33 -0800

    Added SetShips

---
+++ BattleBoats/BattleBoats/BattleBoats/BattleBoats/classes.swift
@@ -0,0 +1,11 @@
+//
+//  classes.swift
+//  BattleBoats
+//
+//  Created by Austin Way on 2/12/21.
+//
+
+import Foundation
+
+// Use this for things we need to save to the device
+struct Storage {}
